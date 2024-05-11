8c102ef7ff3365fb1a27f9cc56fe69a03a405a33
Saurabh <sygirme13@gmail.com>
2024-05-11 20:53:47 +0530

    function for deleteing a task from our todo list implemented and tested successfully

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react'
 import {AiOutlinePlus} from 'react-icons/ai'
 import Todo from './Todo'
 import {db} from './firebase'
-import {query, collection, onSnapshot, updateDoc, doc, addDoc} from 'firebase/firestore'
+import {query, collection, onSnapshot, updateDoc, doc, addDoc, deleteDoc} from 'firebase/firestore'
 
 const style = {
   bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`,
@@ -59,18 +59,33 @@ function App() {
 
   // Delete todo
 
+  const deleteTodo = async (id) => {
+    await deleteDoc(doc(db, 'todos' ,id))
+  }
+
 
   return (
     <div className={style.bg}>
       <div className={style.container}>
         <h3 className={style.heading}>Todo App</h3>
         <form onSubmit={createTodo} className={style.form}>
-          <input value={input} onChange={(e) => setInput(e.target.value)} className={style.input} type="text" placeholder="Add Todo" />
+          <input 
+            value={input} 
+            onChange={(e) => setInput(e.target.value)} 
+            className={style.input} 
+            type="text" 
+            placeholder="Add Todo" 
+          />
           <button className={style.button}><AiOutlinePlus size={30}/></button>
         </form>
         <ul>
           {todos.map((todo, index) => (
-            <Todo key={index} todo={todo} toggleComplete={toggleComplete} />
+            <Todo 
+              key={index} 
+              todo={todo} 
+              toggleComplete={toggleComplete} 
+              deleteTodo={deleteTodo}
+            />
           ))}
         </ul>
 
